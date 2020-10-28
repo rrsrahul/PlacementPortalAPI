8cc415fc83c4ce0462ca54d90d11afe77a151ea8
Rahul R S <rahulrsgoku@gmail.com>
2020-10-29 01:12:17 +0530

    Mongoose Models

---
+++ Models/student.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  
+});
+
+module.exports = mongoose.model('Student', studentSchema);

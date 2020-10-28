dbdf62263779898a9b5b4e4b7c0653cd4cd85250
Rahul R S <rahulrsgoku@gmail.com>
2020-10-28 23:47:48 +0530

    Connection to Database

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const e = require('./exp');
+
+mongoose.connect(e.cs,{ useNewUrlParser: true ,  useUnifiedTopology: true })
+.then(()=>
+{
+    app.listen(3000, ()=>{console.log('server has started')});
+}).catch((err)=>
+{
+    console.log(err)
+});
+
 
 //middlewares
 
 
 
-app.listen(3000);

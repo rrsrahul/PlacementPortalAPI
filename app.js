a6227fa0f2196d90d97f22687a78520a719daf70
Rahul R S <rahulrsgoku@gmail.com>
2020-10-29 18:48:10 +0530

    Using .env

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const e = require('./exp');
 
-mongoose.connect(e.cs,{ useNewUrlParser: true ,  useUnifiedTopology: true });
+
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true ,  useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error',(err)=>{console.log(err)});
 db.once('open',()=>{ console.log('Connected to the DataBase')});
@@ -12,6 +12,7 @@ db.once('open',()=>{ console.log('Connected to the DataBase')});
 app.listen(3000, ()=>{console.log('server has started')});
 
 //middlewares
+
 
 
 
